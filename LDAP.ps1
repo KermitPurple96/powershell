88299bfd15b1b2da1d84a4cd2ce437e471fc3839
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2024-07-13 19:02:32 +0200

    Update LDAP.ps1

--- LDAP.ps1
+++ LDAP.ps1
@@ -1,11 +1,19 @@
-
 $PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
 $DN = ([adsi]'').distinguishedName
 $LDAP = "LDAP://$PDC/$DN"
 $LDAP
+
 $direntry = New-Object System.DirectoryServices.DirectoryEntry($LDAP)
 $dirsearcher = New-Object System.DirectoryServices.DirectorySearcher($direntry)
-$dirsearcher.filter="user=vgarcia"
+
+#objects
+#$dirsearcher.FindAll()
+
+# enum users
+#$dirsearcher.filter="samAccountType=805306368"
+
+# 1 user
+$dirsearcher.filter="name=vgarcia"
 $result = $dirsearcher.FindAll()
 
 Foreach($obj in $result)
