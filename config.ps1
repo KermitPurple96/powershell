b54f19aedcf3485cb4df2151e808d2fa730befee
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2025-01-14 17:15:25 +0100

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,3 +1,4 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 Install-Module -Name Terminal-Icons -Scope CurrentUser -Force
 Install-Module -Name PSReadLine -Scope CurrentUser -Force
+winget install Neovim.Neovim
