53b1e1974131014a243d21e2820a05c637e1a6e7
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2025-01-17 12:48:20 +0100

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -2,3 +2,7 @@ winget install JanDeDobbeleer.OhMyPosh -s winget
 Install-Module -Name Terminal-Icons -Scope CurrentUser -Force
 Install-Module -Name PSReadLine -Scope CurrentUser -Force
 winget install Neovim.Neovim
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
